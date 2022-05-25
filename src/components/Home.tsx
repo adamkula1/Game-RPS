e079ddda4ca9d128c1a711847434d6d6e1ed5d09
Adam Kula <adamkula97@gmail.com>
2022-05-25 21:43:27 +0200

    responsive design and footer button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Triangle from "../assets/images/bg-triangle.svg";
 import Paper from "../assets/images/icon-paper.svg";
+import Footer from './Footer';
 
 const Home = () => {
   return (
-    <main>
-        <section className='choice'>
-            <img className='icon-triangle' src={Triangle} alt="Triangle" />
-            <div className='icon icon-paper'></div>
-            <div className='icon icon-scissors'></div>
-            <div className='icon icon-rock'></div>
-        </section>
-    </main>
+    <>
+      <main>
+          <section className='choice'>
+              <img className='icon-triangle' src={Triangle} alt="Triangle" />
+              <div className='icon icon-paper'></div>
+              <div className='icon icon-scissors'></div>
+              <div className='icon icon-rock'></div>
+          </section>
+      </main>
+      <Footer />
+    </>
   )
 }
 
