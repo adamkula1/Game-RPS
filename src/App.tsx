0479a2771d14471cf08d1885041f58d71270118e
Adam Kula <adamkula97@gmail.com>
2022-05-27 16:41:38 +0200

    playground-design-fix, add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
 import PlayGround from './components/PlayGround'
@@ -9,8 +10,12 @@ const App = () => {
   return (
     <>
       <Header />
-      <Home />
-      <PlayGround />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='playground' element={<PlayGround />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/PlayGround.tsx
+++ src/components/PlayGround.tsx
@@ -4,17 +4,17 @@ const PlayGround = () => {
   return (
     <section className='game'>
         <div className='game-my'>
-            <p>You Picked</p>
+            <p className='text-side'>You Picked</p>
             <div className='icon icon-paper'></div>
         </div>
 
         <div className='game-result'>
-            <p className='text-result'></p>
+            <p className='text-result'>YOU WIIIN</p>
             <a className='play-again' href="/">Play Again</a>
         </div>
 
         <div className='game-pc'>
-            <p>Opponent Picked</p>
+            <p className='text-side'>Opponent Picked</p>
             <div className='icon icon-paper'></div>
         </div>
     </section>
